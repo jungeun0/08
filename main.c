afff4a3eed3f36a3c71fa19d2fc84279031f27f8
jungeun0 <sky_fall7@naver.com>
2018-11-15 16:07:20 +0900

    08-5

--- main.c
+++ main.c
@@ -1,20 +1,66 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
-char *proverb = "All that glisters is not gold.";
+   /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-void setPointer(char **q){
-	
-	*q = proverb;
-	
-	
-}
+	int sum(int a, int b){
+				
+		return a + b;
+	}
+	int sub(int a, int b){
+		
+		return a = b;
+	}
+	int mul(int a, int b){
+		return a * b ;
+		
+	}
+	int nanugi(int a, int b){
+		
+		return a/b ;
+	}
 int main(int argc, char *argv[]) {
 	
-	char *p = "zzz";
-	setPointer(&p);
-	printf("%s\n",p);
+	int i;
+	int j;
+	char k;
+	int (*function)(int,int);
+	
+
+
+	printf("input calculation : ");
+	scanf("%d %s %d",&i, &k, &j);
+	
+	switch(k){
+		
+		case '+' :
+			function = sum;
+			break;
+		case  '-' :
+			function = sub;
+			break;
+		case '*' :
+			function = mul;
+			break;
+		case '/' :
+			function = nanugi;
+			break; 
+		
+		
+	}
+		
+		
+	
+	printf("result : %i\n",function(i,j));	
+	
+	
+	
+	
+	
 	
 	return 0;
 }
+
+
+
+
