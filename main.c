573e8ac618ba700b9a7632eef7f194b3cf398311
jungeun0 <sky_fall7@naver.com>
2018-11-15 15:32:28 +0900

    08-3

--- main.c
+++ main.c
@@ -2,23 +2,20 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
+char *proverb = "All that glisters is not gold.";
 
+void setPointer(char *q){
+	
+	q = proverb;
+	
+	
+}
 int main(int argc, char *argv[]) {
 	
-	char *pc;
-	int *pi;
-	double *pd;
+	char *p = "zzz";
+	setPointer(p);
+	printf("%s\n",p);
 	
-	pc = (char*)10000; //char의 bite 수 
-	pi = (int*)10000; // int의 bite 수 
-	pd = (double*)10000; // double의 bite 수 
-	
-	printf("before : pc=%d, pi=%d, pd=%d\n",pc,pi,pd);
-	
-	pc++;
-	pi++;
-	pd++;
-	printf("after : pc=%d, pi=%d, pd=%d\n",pc,pi,pd);
 	
 	
 	
